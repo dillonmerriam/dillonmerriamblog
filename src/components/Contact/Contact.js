56eaa12ac57be5f1788503ac550743728bf5a223
Dillon Merriam <merriamdillon@yahoo.com>
2022-04-20 02:49:21 -0700

    links to twitter github, new styles

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './contact.scss'
+
+export const Contact = () => {
+  return (
+    <div className='contact-container' id='contact'>
+      <span>Contact</span>
+      <div className='contact-form'>
+        <div className='user-message'>
+          <textarea type="text" id="message" name="message" cols="50" rows="10"/>
+        </div>
+        <div className='user-info'>
+          <form>
+            <label for="fname">First name:</label><br/>
+            <input type="text" id="fname" name="fname"/><br/>
+            <label for="lname">Last name:</label><br/>
+            <input type="text" id="lname" name="lname"/><br/>
+            <label for="email">Email:</label><br/>
+            <input type="text" id="email" name="email"/><br/>
+          </form>
+          <button>Submit</button>
+        </div>
+
+      </div>
+      
+    </div>
+  )
+}
