8ba4919a10d07a4386c0fd0bf88170c1deca7db4
Dillon Merriam <merriamdillon@yahoo.com>
2022-04-21 15:07:35 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import {  Header, Bio, Contact, Blog } from './components/index.js'
 function App() {
   return (
     <div className="App">
-      
-      <Header />
-      <div className="wrapper">
-        <div className="content-wrapper">
-          <Blog />
-          <Contact />
-        </div>
+    <Header /> 
+    <div className="wrapper"> 
+      <div className="content-wrapper">
+        <Blog />
+      </div>
+      <div className="bio-wrapper">
         <Bio />
+      </div> 
+    </div>
+      <div className='contact-wrapper'>
+        <Contact />
       </div>
     </div>
   );
--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -4,23 +4,26 @@ import './bio.scss'
 export const Bio = () => {
   return (
     <div className='bio-container'>
-    <h1 id='title'>
-    About Me
-  </h1>
-        <img src={require('../../assets/ProfPic.jpg')} />
-        <p>Hello, and welcome to my personal website!
-        I am a Web & Applications Developer from Los Angeles, California 
-        with experience in using various technologies such as (but not limited to)
-        HTML, CSS, Javascript, React, Node, SQL, MongoDB, Next, Gatsby, and Preact!
-        </p>
-        <nav>
-          <a href="https://twitter.com/g1itchw0lf" target="_blank">
-            <img src={require('../../assets/twitterLogo.png')} />
-          </a>
-          <a href="https://github.com/dillonmerriam" target="_blank">
-            <img src={require('../../assets/githubLogo.png')} />
-          </a>
-        </nav>
+      <h1 id='title'>
+        About
+      </h1>
+      
+
+        <img src={require('../../assets/ProfPic.jpg')} id='profile-pic'/>
+
+      <p>Los Angeles, Ca</p>
+      <div className='techLogos'>
+        <img src={require('../../assets/techLogos.png')} id='tech-logos'/>
+      </div>
+      <h1>Links</h1>
+      <nav>
+        <a href="https://twitter.com/g1itchw0lf" target="_blank">
+          <img src={require('../../assets/twitterLogo.png')} />
+        </a>
+        <a href="https://github.com/dillonmerriam" target="_blank">
+          <img src={require('../../assets/githubLogo.png')} />
+        </a>
+      </nav>
         
     </div>
   )
